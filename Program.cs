3a1209ed5f994dc82ab629c4115abdd9851ab716
Thanakon.sa <119285334+ThanakonSA@users.noreply.github.com>
2024-11-26 11:26:56 +0700

    Lab1

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+string name = "Saharat Bonnao";
+string student_id = "673450201-5";
+
+// string is Datatype -> =ชนิดข้อมูล
+// name is veriable
+// "..." is value of varible
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine("My name is " + name);
+Console.WriteLine("My student id " + student_id);
